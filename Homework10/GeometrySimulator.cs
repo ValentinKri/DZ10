cd46b626891e5d6fbdf90def292625ce8f9cf0e8
Denis <109104246+Denis0014@users.noreply.github.com>
2023-05-24 18:06:56 +0300

    Perimeter + Area + Rotate + FarthestRectangle

--- Homework10/GeometrySimulator.cs
+++ Homework10/GeometrySimulator.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using static System.Math;
 
 namespace GeometrySimulator
@@ -15,17 +14,20 @@ namespace GeometrySimulator
             Y = y;
         }
 
-        public static double Dist(Point point1, Point point2) => Sqrt(Pow(point1.X - point2.X, 2) + Pow(point1.Y - point2.Y, 2));
+        public static double Dist(Point point1, Point point2) =>
+            Sqrt(Pow(point1.X - point2.X, 2) + Pow(point1.Y - point2.Y, 2));
         public override string ToString() => $"({X}, {Y})";
     }
 
     public class Rectangle
     {
-        public Point Point1 { get; }
-        public Point Point2 { get; }
-        public Point Point3 { get; }
-        public Point Point4 { get; }
-        public Point Center { get; }
+        public Point Point1 { get; private set; }
+        public Point Point2 { get; private set; }
+        public Point Point3 { get; private set; }
+        public Point Point4 { get; private set; }
+        public Point Center { get; private set; }
+
+        public Point[] Points;
 
         public Rectangle(Point point1, Point point2, Point point3, Point point4)
         {
@@ -38,8 +40,63 @@ namespace GeometrySimulator
             double dx = (point3.X - point1.X) / 2;
             double dy = (point3.Y - point1.Y) / 2;
             Center = new Point(point1.X + dx, point1.Y + dy);
+            Points = new Point[4] { Point1, Point2, Point3, Point4 };
         }
 
-        public override string ToString() => $"({Point1}, {Point2}, {Point3}, {Point4})";
+        public void Rotate(double angle)
+        {
+            double angleRadian = angle * PI / 180;
+            for (int i = 0; i < 4; i++)
+            {
+                double x = Round((Points[i].X - Center.X) * Cos(angleRadian) - (Points[i].Y - Center.Y) * Sin(angleRadian) + Center.X, 3);
+                double y = Round((Points[i].X - Center.X) * Sin(angleRadian) + (Points[i].Y - Center.Y) * Cos(angleRadian) + Center.Y, 3);
+                Points[i] = new Point(x, y);
+            }
+            Point1 = Points[0];
+            Point2 = Points[1];
+            Point3 = Points[2];
+            Point4 = Points[3];
+        }
+        public double Perimeter() =>
+            Point.Dist(Point1, Point2) + Point.Dist(Point2, Point3) + Point.Dist(Point3, Point4) + Point.Dist(Point4, Point1);
+        public double Area() =>
+            Point.Dist(Point1, Point2) * Point.Dist(Point1, Point4);
+        public override string ToString() =>
+            $"({Point1}, {Point2}, {Point3}, {Point4})";
+    }
+
+    public class Simulator
+    {
+        public Point Center { get; } = new Point(0, 0);
+        public List<Rectangle> Rectangles;
+
+        public Simulator(IEnumerable<Rectangle> rectangles)
+        {
+            Rectangles = rectangles.ToList();
+        }
+
+        public Rectangle? FarthestRectangle()
+        {
+            double maxDist = double.MaxValue;
+            Rectangle? res = null;
+            foreach (var rectangle in Rectangles)
+            {
+                double nearest = double.MaxValue;
+                foreach (var point in rectangle.Points)
+                {
+                    if (nearest < Point.Dist(Center, point))
+                    {
+                        nearest = Point.Dist(Center, point);
+                    }
+                }
+                if (nearest > maxDist)
+                {
+                    maxDist = nearest;
+                    res = rectangle;
+                }
+            }
+            return res;
+        }
+        public void Print() => string.Join("\n", Rectangles);
     }
 }
--- Homework10/Program.cs
+++ Homework10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GeometrySimulator;
 
 namespace Homework10
 { 
@@ -6,6 +7,10 @@ namespace Homework10
     {
         static void Main(string[] args)
         {
+            var c = new Rectangle(new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0));
+            Console.WriteLine(c.ToString());
+            c.Rotate(45);
+            Console.WriteLine(c.ToString());
         }
     }
 }
